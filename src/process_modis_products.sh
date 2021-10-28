d269fe49bf2f95d39b849c6ab3d3dd651982a6b2
khufkens <koen.hufkens@gmail.com>
2021-10-28 16:34:24 +0200

    check repo location

--- src/process_modis_products.sh
+++ src/process_modis_products.sh
@@ -3,6 +3,14 @@
 # set bundles
 bundles="modis_lst modis_fpar modis_evi modis_ndiv"
 
+# check dirs
+pwd=`pwd`
+
+if [ ${pwd} -ne "flux_data_kit"]; then
+   echo "You are not executing the script in the main project directory, exiting!"
+   exit 1
+fi
+
 for i in $bundles; do
 echo "Submitting bundle $i ..."
 bsub -W 72:00 -u $USER -J "format $i" -R "rusage[mem=48000]" "Rscript --vanilla ./data/download_modis.R $i"
