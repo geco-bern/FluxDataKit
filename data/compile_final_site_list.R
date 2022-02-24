aae3b0b19564733f0dc0d6f2b74a8d577b6dd41c
khufkens <koen.hufkens@gmail.com>
2022-02-24 19:47:44 +0100

    changes

--- data/compile_final_site_list.R
+++ data/compile_final_site_list.R
@@ -2,14 +2,66 @@
 library(tidyverse)
 
 # read site data RDS files
-
 # append site types (ICOS, PLUMBER etc)
-
-
 # sort through the sites lists depending on the
 # order or preference of the data
 
 # 1. ICOS X
+icos <- readRDS("data/icos_meta-data.rds") %>%
+  rename(
+    'sitename' = 'id',
+    'elev' = 'elevation'
+  ) %>%
+  select(
+    sitename, lat, lon, elev
+  )
+nrow(icos)
+
 # 2. OneFlux
+oneflux <- readRDS("data/oneflux_meta-data.rds") %>%
+  filter(
+    DATA_POLICY != "LEGACY"
+  ) %>%
+  rename(
+    'sitename' = 'SITE_ID',
+    'lat' = 'LOCATION_LAT',
+    'lon' = 'LOCATION_LONG',
+    'elev' = 'LOCATION_ELEV',
+    'date_start' = 'DATA_START',
+    'date_end' = 'DATA_END',
+    'koeppen_code' = 'CLIMATE_KOEPPEN'
+  ) %>%
+  select(
+    sitename, lat, lon, elev, koeppen_code, date_start, date_end
+  )
+
+nrow(oneflux)
+
 # 3. ameriflux
+ameriflux <- readRDS("data/amf_meta-data.rds") %>%
+  filter(
+    DATA_POLICY != "LEGACY"
+  ) %>%
+  rename(
+    'sitename' = 'SITE_ID',
+    'lat' = 'LOCATION_LAT',
+    'lon' = 'LOCATION_LONG',
+    'elev' = 'LOCATION_ELEV',
+    'date_start' = 'DATA_START',
+    'date_end' = 'DATA_END',
+    'koeppen_code' = 'CLIMATE_KOEPPEN'
+  ) %>%
+  select(
+    sitename, lat, lon, elev, koeppen_code, date_start, date_end
+  )
+
+nrow(ameriflux)
+
 # 4. plumber
+plumber <- readRDS("data/plumber_meta-data.rds")
+nrow(plumber)
+
+# Merging routine
+df <- icos
+
+
