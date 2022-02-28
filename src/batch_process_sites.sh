fd5c6a68e0a974caabb2439f7cb39bb7c39bf158
khufkens <koen.hufkens@gmail.com>
2022-02-28 16:39:21 +0100

    don't run model

--- src/batch_process_sites.sh
+++ src/batch_process_sites.sh
@@ -18,6 +18,6 @@ fi
 
 for ((n=1;n<=${njobs};n++)); do
    echo "Submitting pixel $n ..."
-   bsub -W 72:00 -u $USER -J "format cell_${n}" -R "rusage[mem=48000]" "Rscript --vanilla ./data-raw/rscript_format_site_data.R $n ${njobs} TRUE"
+   bsub -W 72:00 -u $USER -J "format cell_${n}" -R "rusage[mem=48000]" "Rscript --vanilla ./data-raw/rscript_format_site_data.R $n ${njobs} FALSE"
 
 done
