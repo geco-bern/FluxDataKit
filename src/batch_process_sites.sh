5e44093c5ae66dfccef1f3ad031cb46d90c9a41d
khufkens <koen.hufkens@gmail.com>
2022-03-08 16:50:52 +0100

    batch script

--- src/batch_process_sites.sh
+++ src/batch_process_sites.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-# jobs to run
-njobs=10
-
-# check dirs
-pwd=`pwd`
-base=`basename $pwd`
-
-for ((n=1;n<=${njobs};n++)); do
-   echo "Submitting pixel $n ..."
-   bsub -W 72:00 -u $USER -J "format cell_${n}" -R "rusage[mem=48000]" "Rscript --vanilla ./data-raw/rscript_format_site_data.R $n ${njobs} FALSE"
-
-done
+echo "flux formatting"
+bsub -W 72:00 -u $USER -J "grabbing flux data" -R "rusage[mem=48000]" "Rscript --vanilla ./data-raw/rscript_format_site_data.R"
