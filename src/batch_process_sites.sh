61050a3d5826ba95e2bcf26a269bca7ca5c44640
khufkens <koen.hufkens@gmail.com>
2022-03-16 14:43:04 +0100

    mem

--- src/batch_process_sites.sh
+++ src/batch_process_sites.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "flux formatting"
-bsub -W 72:00 -u $USER -J "grabbing flux data" -R "rusage[mem=48000]" "Rscript --vanilla ./data-raw/rscript_format_site_data.R hh"
+bsub -W 72:00 -u $USER -J "grabbing flux data" -R "rusage[mem=96000]" "Rscript --vanilla ./data-raw/rscript_format_site_data.R hh"
