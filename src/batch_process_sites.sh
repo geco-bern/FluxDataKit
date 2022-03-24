0aa983b4b09c12ca25f7f85b273ba56faa0cb497
khufkens <koen.hufkens@gmail.com>
2022-03-24 10:16:45 +0100

    dynamic batch script

--- src/batch_process_sites.sh
+++ src/batch_process_sites.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "processing time step $1"
+
 echo "flux formatting"
-bsub -W 72:00 -u $USER -J "grabbing flux data" -R "rusage[mem=96000]" "Rscript --vanilla ./data-raw/rscript_format_site_data.R hh"
+bsub -W 72:00 -u $USER -J "grabbing flux data" -R "rusage[mem=96000]" "Rscript --vanilla ./data-raw/rscript_format_site_data.R $1"
