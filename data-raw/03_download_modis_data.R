9acce4fa3ebcead7ee7f3c77ff35ba97da9afa81
khufkens <koen.hufkens@gmail.com>
2022-04-21 14:30:46 +0200

    rename

---
+++ data-raw/03_download_modis_data.R
@@ -0,0 +1,50 @@
+#!/usr/bin/env Rscript
+
+# Updates MODIS subsets on Euler
+#
+# This is run for Ameriflux data
+# but can be appended with other datasets
+#
+# Assumes that your local (home dir)
+# data directory links to the CES Euler
+# data repository as mentioned in the notions.co
+# setup page for Euler
+#
+
+# load script arguments
+args <- commandArgs(trailingOnly=TRUE)
+
+# load required libraries
+library(ingestr)
+
+# set sites
+sites <- readRDS("data/flux_data_kit_site-info.rds")
+sites$year_start <- 2000
+sites$year_end <- as.numeric(format(Sys.Date(),"%Y"))
+bundle <- args[1]
+
+# feedback
+message("processing GEE data product: ")
+
+# feedback
+message(paste("  - ", bundle))
+
+settings_gee <- get_settings_gee(
+  bundle            = bundle,
+  python_path       = "/usr/bin/python3",
+  gee_path          = "./src/gee_subset/src/gee_subset",
+  data_path         = "~/data/gee_subsets/",
+  method_interpol   = "none",
+  keep              = TRUE,
+  overwrite_raw     = TRUE,
+  overwrite_interpol= TRUE
+)
+
+# run the ingest routine
+df_modis <- ingest(
+  sites,
+  source = "gee",
+  settings = settings_gee,
+  parallel = FALSE
+)
+
