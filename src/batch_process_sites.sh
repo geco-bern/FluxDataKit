a02ffb0fa55f2cc0e2629238f8dd6f1b8ae5f91e
khufkens <koen.hufkens@gmail.com>
2022-04-21 15:11:13 +0200

    remove euler specific scripts

--- src/batch_process_sites.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "processing time step $1"
-
-echo "flux formatting"
-bsub -W 72:00 -u $USER -J "grabbing flux data" -R "rusage[mem=96000]" "Rscript --vanilla ./data-raw/rscript_format_site_data.R $1"
