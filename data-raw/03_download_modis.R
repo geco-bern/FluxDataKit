7a770fa9ea41a256434d53cc41de6c781c163e7d
khufkens <koen.hufkens@gmail.com>
2022-04-21 15:04:31 +0200

    correct download routine

--- data-raw/03_download_modis.R
+++ data-raw/03_download_modis.R
@@ -1,18 +1,5 @@
-#!/usr/bin/env Rscript
-
-# Updates MODIS subsets on Euler
-#
-# This is run for Ameriflux data
-# but can be appended with other datasets
-#
-# Assumes that your local (home dir)
-# data directory links to the CES Euler
-# data repository as mentioned in the notions.co
-# setup page for Euler
-#
-
-# load script arguments
-args <- commandArgs(trailingOnly=TRUE)
+# Should be run as a job
+# or a script on the command line
 
 # load required libraries
 library(ingestr)
@@ -21,31 +8,58 @@ library(ingestr)
 sites <- readRDS("data/flux_data_kit_site-info.rds")
 sites$year_start <- 2000
 sites$year_end <- as.numeric(format(Sys.Date(),"%Y"))
-bundle <- args[1]
 
-# feedback
-message("processing MODIS data product: ")
+bundles <-
+  c(
+    "modis_lst_aqua",
+    "modis_lst_terra",
+    "modis_fpar",
+    "modis_lai",
+    "modis_gpp",
+    "modis_refl_1",
+    "modis_refl_2",
+    "modis_refl_3",
+    "modis_refl_4",
+    "modis_refl_5",
+    "modis_refl_6",
+    "modis_refl_7",
+    "modis_refl_8",
+    "modis_refl_9",
+    "modis_refl_10",
+    "modis_refl_11",
+    "modis_refl_12",
+    "modis_refl_13",
+    "modis_refl_14",
+    "modis_refl_15",
+    "modis_refl_16"
+  )
 
-# feedback
-message(paste("  - ", bundle))
+lapply(bundles, function(bundle){
+  # feedback
+  message("processing MODIS data product: ")
 
-# grab settings
-settings_modis <- get_settings_modis(
-  bundle            = bundle,
-  data_path         = "~/data/modis_subsets/",
-  method_interpol   = "loess",
-  keep              = TRUE,
-  overwrite_raw     = TRUE,  # not too costly when using networks
-  overwrite_interpol= TRUE,
-  n_focal           = 0,
-  network           = "FLUXNET"
-)
+  # feedback
+  message(paste("  - ", bundle))
 
-# run the ingest routine
-df_modis <- ingest(
-  sites,
-  source = "modis",
-  settings = settings_modis,
-  parallel = FALSE
-)
+  # grab settings
+  settings_modis <- get_settings_modis(
+    bundle            = bundle,
+    data_path         = "data-raw/modis/",
+    method_interpol   = "loess",
+    keep              = TRUE,
+    overwrite_raw     = FALSE,  # not too costly when using networks
+    overwrite_interpol= TRUE,
+    n_focal           = 0,
+    network           = "FLUXNET"
+  )
 
+  # run the ingest routine
+  df_modis <- ingest(
+    sites,
+    source = "modis",
+    settings = settings_modis,
+    parallel = FALSE
+  )
+
+  return(invisible())
+})
