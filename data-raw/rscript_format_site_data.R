ea4864bfd4d97ec30bba16db2f20f5ccfe38206d
khufkens <koen.hufkens@gmail.com>
2022-04-27 15:26:17 +0200

    formatting script

--- data-raw/rscript_format_site_data.R
+++ data-raw/rscript_format_site_data.R
@@ -6,14 +6,12 @@ freq <- args[1]
 # scripts
 library(tidyverse)
 library(ingestr)
-library(rsofun)
-
+source("R/collect_drivers_rsofun.R")
 source("R/format_site_drivers.R")
+source("R/prepare_setup_sofun.R")
 
 # read sites data frame
 df_sites <- readRDS("data/flux_data_kit_site-info.rds")
-# %>%
-#   dplyr::select(sitename, lat, lon, year_start, year_end, elv, product)
 
 data <- df_sites %>%
   rowwise() %>%
@@ -60,4 +58,3 @@ if (freq == "hh"){
     "data/p_model_drivers/site_based_drivers.rds",
     compress = "xz")
 }
-
