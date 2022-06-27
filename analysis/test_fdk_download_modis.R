267a5e2ec3f1845289cdf6e60b404f80f3f02cd8
khufkens <koen.hufkens@gmail.com>
2022-06-27 15:24:22 +0200

    debugging scripts

---
+++ analysis/test_fdk_download_modis.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(dplyr)
+library(FluxnetLSM)
+source("R/fdk_process_lsm.R")
+source("R/fdk_download_modis.R")
+
+# select datasets / sites to process
+datasets <- c("fluxnet2015","icos","oneflux")
+sites <- readRDS("data/flux_data_kit_site-info.rds") %>%
+  filter(
+    product != "plumber",
+    sitename == "AT-Neu"
+  )
+
+fdk_download_modis(df = sites)
