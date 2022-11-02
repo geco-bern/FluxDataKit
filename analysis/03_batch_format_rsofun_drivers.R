f9705a0ac78ea66d0c489fdd4633f72076f2c04e
Koen Hufkens <koen.hufkens@gmail.com>
2022-11-02 15:33:38 +0100

    batch processing script rsofun

---
+++ analysis/03_batch_format_rsofun_drivers.R
@@ -0,0 +1,70 @@
+# Suppress summarise info
+options(tidyverse.quiet = TRUE)
+options(dplyr.summarise.inform = FALSE)
+
+library(tidyverse)
+library(ingestr)
+library(FluxDataKit)
+
+sites <- readRDS("data/flux_data_kit_site-info.rds")
+
+driver_data <- lapply(sites$sitename[1:2], function(site){
+
+  message(sprintf("Processing %s ----", site))
+
+  message("- converting to FLUXNET format")
+  df <- suppressWarnings(try(fdk_convert_lsm(
+    site = site,
+    fluxnet_format = TRUE,
+    path = "/data/scratch/PLUMBER_X/"
+    )
+  ))
+
+  if(inherits(df, "try-error")){
+    message("!!! conversion to FLUXNET failed  !!!")
+    return(NULL)
+  }
+
+  message("- downsampling FLUXNET format")
+  filename <- suppressMessages(
+    suppressWarnings(
+      fdk_downsample_fluxnet(
+        df,
+        site = site,
+        out_path = tempdir()
+      )
+    )
+  )
+
+  if(inherits(filename, "try-error")){
+    message("!!! downsampling failed !!!")
+    return(NULL)
+  }
+
+  message("- compiling drivers")
+  # Use a uniform FLUXNET HH input
+  # file to generate p-model (rsofun)
+  # compatible driver data
+  output <- suppressMessages(
+    suppressWarnings(
+      fdk_format_drivers(
+        site_info = sites |> filter(sitename == !!site),
+        freq = "d",
+        path = paste0(tempdir(),"/"), # f-ing trailing /
+        verbose = TRUE
+      )
+    )
+  )
+
+  if(inherits(output, "try-error")){
+    message("!!! formatting drivers failed  !!!")
+    return(NULL)
+  } else {
+    return(output)
+  }
+})
+
+# bind all tibbles
+driver_data <- dplyr::bind_rows(driver_data)
+
+
