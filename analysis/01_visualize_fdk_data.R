8ffa7f05b26f2e5717cb0b0c614da90d3048dc72
Koen Hufkens <koen.hufkens@gmail.com>
2022-11-07 19:48:33 +0100

    new plot generation routine

--- analysis/01_visualize_fdk_data.R
+++ analysis/01_visualize_fdk_data.R
@@ -1,16 +1,49 @@
+
+# load libraries
 library(tidyverse)
 library(FluxDataKit)
+lapply(list.files("R/","*", full.names = TRUE), source)
 
-source("R/fdk_plot.R")
+# load sites
+sites <- readRDS("data/flux_data_kit_site-info.rds")
 
-files <- list.files("/data/scratch/PLUMBER_X/fluxes/","*Flux.nc", full.names = TRUE)
+# loop over all sites and plot all time series
+failed_sites <- lapply(sites$sitename, function(site){
+  message(sprintf("Processing %s ----", site))
 
-lapply(files, function(file){
-  try(fdk_plot(
-    file = file,
-    out_path = "/data/scratch/PLUMBER_X/plots/",
-    overwrite = FALSE
+  message("- converting to FLUXNET format")
+  df <- suppressWarnings(try(fdk_convert_lsm(
+    site = site,
+    fluxnet_format = TRUE,
+    path = "/data/scratch/PLUMBER_X/"
+  )
+  ))
+
+  if(inherits(df, "try-error")){
+    message("!!! conversion to FLUXNET failed  !!!")
+    return(site)
+  }
+
+  message("- plotting FLUXNET data")
+  filename <- suppressMessages(
+    suppressWarnings(
+      try(fdk_plot(
+        df,
+        site = site,
+        out_path = "/data/scratch/fluxnet_plots/",
+        overwrite = TRUE
+      )
+      )
     )
   )
-  invisible()
+
+  if(inherits(filename, "try-error")){
+    message("!!! downsampling failed !!!")
+    return(site)
+  }
+
+  return(NULL)
 })
+
+failed_sites <- do.call("rbind", failed_sites)
+print(failed_sites)
