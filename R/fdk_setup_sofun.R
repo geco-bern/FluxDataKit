5ef7dced7d748030821d0ab52eb359b7e445a47e
Koen Hufkens <koen.hufkens@gmail.com>
2022-11-11 16:24:36 +0100

    update pipe

--- R/fdk_setup_sofun.R
+++ R/fdk_setup_sofun.R
@@ -11,7 +11,6 @@
 #' complemented by column 'params_siml' which is a nested list
 #' of complemented simulation parameters.
 #' @export
-#' @importFrom magrittr %>%
 #' @examples
 #' \dontrun{
 #'  setup <- prepare_setup_sofun(
@@ -53,24 +52,24 @@ fdk_setup_sofun <- function(
   ##--------------------------------------
   ## Complement settings with meta info for each site
   ##--------------------------------------
-  site_info <- site_info %>%
+  site_info <- site_info |>
 
     ## clean up and complement
     mutate(year_start = as.numeric(year_start),
-           year_end = as.numeric(year_end)) %>%
+           year_end = as.numeric(year_end)) |>
     mutate(date_start = lubridate::ymd(
              paste0( as.character( site_info$year_start ), "-01-01" ) ),
            date_end   = lubridate::ymd(
              paste0( as.character( site_info$year_end ), "-12-31" ) ))
 
   ## add simulation parameters as a list nested in 'site_info'
-  site_info <- params_siml %>%
-    dplyr::bind_cols(.) %>%
-    dplyr::mutate(tmp = 1) %>%
-    dplyr::right_join(mutate(site_info, tmp = 1), by = "tmp") %>%
-    dplyr::select(names(site_info), names(params_siml)) %>%
-    dplyr::mutate(nyeartrend = year_end - year_start + 1) %>%
-    dplyr::rename(firstyeartrend = year_start) %>%
+  site_info <- params_siml |>
+    dplyr::bind_cols(.) |>
+    dplyr::mutate(tmp = 1) |>
+    dplyr::right_join(mutate(site_info, tmp = 1), by = "tmp") |>
+    dplyr::select(names(site_info), names(params_siml)) |>
+    dplyr::mutate(nyeartrend = year_end - year_start + 1) |>
+    dplyr::rename(firstyeartrend = year_start) |>
     tidyr::nest(params_siml = c(names(params_siml),
                                 "firstyeartrend", "nyeartrend"))
 
