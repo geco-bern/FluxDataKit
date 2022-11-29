18efab418e75d970a464a11a467d6d00f4669dd6
Koen Hufkens <koen.hufkens@gmail.com>
2022-11-29 13:50:28 +0100

    documenting smoothing function

--- R/fdk_smooth_ts.R
+++ R/fdk_smooth_ts.R
@@ -28,15 +28,18 @@ fdk_smooth_ts <- function(
       )
 
     # detect outliers
-    df <- suppressWarnings(fdk_detect_outliers(
+    df <- suppressWarnings(
+      fdk_detect_outliers(
       df = df,
       sigma = 1,
       plot = FALSE
-    ))
+      )
+    )
 
-    print(head(df))
-
-    # gap fill and smooth
+    # get mean annual cycle
+    # and linearly interpolate and
+    # carry forward or backward any
+    # remaining gaps
     mean_df <- df |>
       mutate(
         values = ifelse(outlierflag == 1, NA, values)
@@ -51,18 +54,26 @@ fdk_smooth_ts <- function(
         mean_values = zoo::na.locf(mean_values, fromLast = TRUE, na.rm = FALSE)
       )
 
-    df <- left_join(df, mean_df) |>
+    # join with original data, and create
+    # combined results (filling NA values in the
+    # original)
+    df <- left_join(df, mean_df, by = "doy") |>
       mutate(
         combined_values = ifelse(is.na(values) | outlierflag == 1, mean_values, values),
         weights = ifelse(is.na(values), 0.1, 1)
       )
 
+    # calculate the optimal span based on
+    # a BIC metric (phenocam approach)
     span <- fdk_optimal_span(
       y = df$combined_values,
       x = as.numeric(df$date),
       weights = df$weights
     )
 
+    # fit the model wiht the optimal
+    # span and export the smooth
+    # time series (see modis_tseries below)
     fit <- stats::loess(
       combined_values ~ as.numeric(date),
       span = span,
@@ -70,17 +81,7 @@ fdk_smooth_ts <- function(
       data = df
     )
 
-    df$smooth <- suppressWarnings(
-      stats::predict(
-        fit,
-        as.numeric(df$date),
-        se = FALSE)
-      )
-
-    plot(df$values, type = "l", col = "red")
-    points(df$smooth, type = "l")
-
-
+    #---- write data to netcdf file ----
     # open netcdf file for writing
     site_nc <- ncdf4::nc_open(nc_file, write = TRUE)
 
@@ -93,6 +94,8 @@ fdk_smooth_ts <- function(
     start_year    <- as.numeric(substr(site_start_time, start=15, stop=18))
     site_time <- as.Date(as.POSIXct(site_time, origin = sprintf("%s-01-01", start_year)))
 
+    # predict values using the fit
+    # model
     modis_tseries <- suppressWarnings(
       stats::predict(
         fit,
