8c279c694814bb6788e85470eb9053eefb5c8ce7
Koen Hufkens <koen.hufkens@gmail.com>
2023-01-31 13:09:04 +0100

    batch download ERA5 cloud cover

--- R/fdk_download_cloud_cover.R
+++ R/fdk_download_cloud_cover.R
@@ -17,33 +17,42 @@ fdk_download_cloud_cover <- function(
 
   #----- settings and startup -----
 
-  df$filename <- paste0(df$sitename, "_", df$variable,"_", df$method,".csv")
-  df$date_start <- paste0(df$year_start,"-01-01")
-  df$date_end <- paste0(df$year_end,"-12-31")
-
   # download all drivers, use site info
   # to determine locality etc
   requests <- apply(df, 1, function(x){
-    output <- fdk_era5_request(
-      lon = as.numeric(x['lon']),
-      lat = as.numeric(x['lat']),
-      filename = x['filename'],
-      start_date = x['date_start'],
-      end_date = x['date_end']
-    )
-    return(output)
+    lapply(seq(as.numeric(x['year_start']), 2022, 5), function(year){
+
+      filename <- paste0(
+        x['sitename'],
+        "_",
+        year,
+        ".csv"
+        )
+
+      output <- fdk_era5_request(
+        lon = as.numeric(x['lon']),
+        lat = as.numeric(x['lat']),
+        filename = filename,
+        start_date = paste0(as.numeric(year), "-01-01"),
+        end_date = paste0(as.numeric(year) + 4, "-12-31")
+      )
+      return(output)
+    })
   })
+
+  requests <- do.call("rbind", requests)
 
   # download the data
   files <- ecmwfr::wf_request_batch(
     user = user,
     requests,
-    time_out = 3 * 3600,
     workers = 2,
     path = path
   )
 
   if(inherits(files, "try-error")) {
     message("Cloud cover downloads failed!")
+  } else {
+    return(files)
   }
 }
--- R/fdk_era5_request.R
+++ R/fdk_era5_request.R
@@ -20,32 +20,17 @@ fdk_era5_request <- function(
     end_date
 ) {
 
-request <- list(
-  product_type = "reanalysis",
-  format = "netcdf",
-  variable = "total_cloud_cover",
-  date = paste(start_date, end_date, sep = "/"),
-  area = c(lat + 0.05, lon - 0.05, lat - 0.05, lon + 0.05),
-  time = c("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00",
-           "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00",
-           "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00",
-           "21:00", "22:00", "23:00"),
-  dataset_short_name = "reanalysis-era5-single-levels",
-  target = filename
-)
-
-  # A query for 2m surface temperature
   request <- list(
-    code = code,
-    kwargs = list(
-      lon = lon,
-      lat = lat,
-      var = var,
-      product = product,
-      date = paste(start_date, end_date, sep = "/"),
-      method = method
-    ),
-    workflow_name = "daily_data",
+    product_type = "reanalysis",
+    format = "netcdf",
+    variable = "total_cloud_cover",
+    date = paste(start_date, end_date, sep = "/"),
+    area = c(lat + 0.05, lon - 0.05, lat - 0.05, lon + 0.05),
+    time = c("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00",
+             "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00",
+             "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00",
+             "21:00", "22:00", "23:00"),
+    dataset_short_name = "reanalysis-era5-single-levels",
     target = filename
   )
 
