cc00f4c37cf128f1aac95133be1808b30112f541
Koen Hufkens <koen.hufkens@gmail.com>
2023-02-06 14:14:55 +0100

    data coverage routine

--- R/fdk_data_coverage.R
+++ R/fdk_data_coverage.R
@@ -4,8 +4,8 @@
 #'
 #' @param input_path path with half-hourly gap-filled plumber formatted data
 #' @param output_path where to store the converted data if converted to csv
-#' @param internal returna an internal data frame or a csv written to file
-#'  (either TRUE or FALSE)
+#' @param internal returns an internal data frame or a csv written to file
+#' named 'data_coverage.csv' (either TRUE or FALSE)
 #'
 #' @return data frame or csv written to disk with data coverage statistics
 #'  per site
@@ -14,7 +14,7 @@
 fdk_data_coverge <- function(
     input_path = "/data/scratch/beta-v3/lsm/",
     output_path,
-    internal = FALSE
+    internal = TRUE
 ){
 
   min_nr <- function(x) {min(which(x != is.na(x)))}
@@ -36,23 +36,47 @@ fdk_data_coverge <- function(
       )
     )
 
+    # date vector
+    date <- as.Date(strptime(tmp$TIMESTAMP_START, "%Y%m%d%H%M", tz = "GMT"))
+
     # remove QC/SE/ERA5 columns
     # these should not be indexed
     tmp <- tmp |>
       select(
         -ends_with("QC"),
         -ends_with("SE"),
-        -ends_with("ERA")
+        -ends_with("ERA"),
+        -starts_with("TIME"),
       )
 
-    print(tmp)
+    tmp <- tmp |>
+      summarise_all(
+        list(
+          start = min_nr,
+          end = max_nr
+        )
+      ) |>
+      mutate_all(
+        function(x){date[x]}
+      )
+
+    tmp$sitename <- site
     return(tmp)
   })
 
   # merge all
   df <- dplyr::bind_rows(df)
 
-  print(head(df))
+  if(!internal){
+    write.table(
+      df,
+      file = file.path(output_path, "data_coverage.csv"),
+      quote = FALSE,
+      row.names = FALSE,
+      col.names = TRUE,
+      sep = ","
+    )
+  } else {
+    return(df)
+  }
 }
-
-fdk_data_coverge()
