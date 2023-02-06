9d6b4a10ee4b338f5f350be872c3ff1dce72e17c
Koen Hufkens <koen.hufkens@gmail.com>
2023-02-06 10:34:06 +0100

    calculate data coverage

---
+++ R/fdk_data_coverage.R
@@ -0,0 +1,22 @@
+#' Calculates data coverage
+#'
+#' Calculates data coverage for the half-hourly (HH) data.
+#'
+#' @param path path with half-hourly gap-filled fluxnet formatted data
+#' @param internal returna an internal data frame or a csv written to file
+#'  (either TRUE or FALSE)
+#' @param out_path where to store the converted data if converted to csv.
+#'
+#' @return data frame or csv written to disk with data coverage statistics
+#'  per site
+#' @export
+
+fdk_data_coverge <- function(
+    path,
+    internal = FALSE,
+    out_path
+){
+
+
+
+}
