e9003b6269ad51fe46a55e79f55ebefe3bb2eea7
Beni Stocker <benjamin.stocker@gmail.com>
2024-03-14 23:02:05 +0100

    revised site meta information and determination of preferred source

--- data-raw/02_compile_final_site_list.R
+++ data-raw/02_compile_final_site_list.R
@@ -1,150 +1,207 @@
 # load libraries
 library(dplyr)
 library(raster)
-library(MODISTools)
+# library(MODISTools)
 
 # read site data RDS files
 # append site types (ICOS, PLUMBER etc)
 # sort through the sites lists depending on the
 # order or preference of the data
 
-# 1. ICOS X
-icos <- readRDS("data-raw/meta_data/icos_meta-data.rds") |>
-  rename(
-    'elev' = 'elevation',
-    'date_start' = 'year_start',
-    'date_end' = 'year_end',
-    'elv' = 'elevation'
-  ) |>
+# Read collected meta data------------------------------------------------------
+## Plumber ----------------------------------------------------------------------
+plumber <- readRDS(here::here("data-raw/meta_data/plumber_meta-data.rds")) |>
+  as_tibble() |>
   dplyr::select(
     sitename,
-    lat,
-    lon,
-    elv,
-    date_start,
-    date_end
+    lon = longitude,
+    lat = latitude,
+    elv = elevation,
+    year_start,
+    year_end,
+    classid = IGBP_veg_short,
+    canopy_height,
+    reference_height
   ) |>
   mutate(
-    product = "icos"
+    year_start = as.numeric(year_start),
+    year_end = as.numeric(year_end)) |>
+  mutate(
+    koeppen_code = NA,
+    nyears = year_end - year_start + 1,
+    product = "plumber"
   )
 
-# 2. OneFlux
-oneflux <- readRDS("data-raw/meta_data/oneflux_meta-data.rds") |>
+## Ameriflux---------------------------------------------------------------------
+ameriflux <- readRDS(here::here("data-raw/meta_data/amf_meta_data.rds")) |>
   filter(
     DATA_POLICY != "LEGACY"
   ) |>
-  rename(
-    'sitename' = 'SITE_ID',
-    'lat' = 'LOCATION_LAT',
-    'lon' = 'LOCATION_LONG',
-    'elv' = 'LOCATION_ELEV',
-    'date_start' = 'DATA_START',
-    'date_end' = 'DATA_END',
-    'koeppen_code' = 'CLIMATE_KOEPPEN'
-  ) |>
   dplyr::select(
-    sitename,
-    lat,
-    lon,
-    elv,
-    koeppen_code,
-    date_start,
-    date_end
+    sitename = site,
+    lon = LOCATION_LONG,
+    lat = LOCATION_LAT,
+    elv = LOCATION_ELEV,
+    year_start = DATA_START,
+    year_end = DATA_END,
+    classid = IGBP,
+    koeppen_code = CLIMATE_KOEPPEN
   ) |>
   mutate(
-    product = "oneflux"
+    canopy_height = NA,
+    reference_height = NA,
+    product = "ameriflux",
+    nyears = year_end - year_start + 1,
   )
 
-# 3. fluxnet / ameriflux
-
-fluxnet <- readRDS("data-raw/meta_data/fluxnet_meta-data.rds") |>
-  rename(
-    'lat' = 'latitude',
-    'lon' = 'longitude',
-    'elv' = 'altitude',
-    'date_start' = 'year_start',
-    'date_end' = 'year_end'
-  ) |>
+## ICOS Drought 2018 release ---------------------------------------------------
+icos_drought2018 <- readRDS(here::here("data-raw/meta_data/icos_drought2018_meta_data.rds")) |>
   dplyr::select(
-    sitename,
-    lat,
+    sitename = site,
     lon,
-    elv,
-    date_start,
-    date_end
+    lat,
+    elv = elevation,
+    year_start,
+    year_end
   ) |>
   mutate(
-    product = "fluxnet2015"
+    classid = NA,
+    koeppen_code = NA,
+    canopy_height = NA,
+    reference_height = NA,
+    product = "icos_drought2018",
+    nyears = year_end - year_start + 1,
   )
 
-# 4. plumber
-plumber <- readRDS("data-raw/meta_data/plumber_meta-data.rds") |>
-  rename(
-    'lat' = 'latitude',
-    'lon' =  'longitude',
-    'elv' = 'elevation',
-    'date_start' = 'year_start',
-    'date_end' = 'year_end'
-  ) |>
+## ICOS Warm Winter 2020 release ---------------------------------------------------
+icos_warmwinter2020 <- readRDS(here::here("data-raw/meta_data/icos_warmwinter2020_meta_data.rds")) |>
   dplyr::select(
-    sitename,
-    lat,
+    sitename = site,
     lon,
-    elv,
-    date_start,
-    date_end
-  )|>
-  mutate(
-    product = "plumber"
+    lat,
+    elv = elevation,
+    year_start,
+    year_end
   ) |>
   mutate(
-    date_start = as.numeric(date_start),
-    date_end = as.numeric(date_end)
+    classid = NA,
+    koeppen_code = NA,
+    canopy_height = NA,
+    reference_height = NA,
+    product = "icos_warmwinter2020",
+    nyears = year_end - year_start + 1,
   )
 
-# Merging routine
-df <- icos
+# ## FLUXNET2015 ------------------------------------------------------------------
+# fluxnet <- readRDS(here::here("data-raw/meta_data/fluxnet_meta_data.rds")) |>
+#   rename(
+#     'lat' = 'latitude',
+#     'lon' = 'longitude',
+#     'elv' = 'altitude',
+#     'date_start' = 'year_start',
+#     'date_end' = 'year_end'
+#   ) |>
+#   dplyr::select(
+#     sitename,
+#     lat,
+#     lon,
+#     elv,
+#     date_start,
+#     date_end
+#   ) |>
+#   mutate(
+#     product = "fluxnet2015"
+#   )
 
-oneflux <- oneflux |>
-  filter(
-    !(sitename %in% df$sitename)
-  )
+# Determine longest source per site --------------------------------------------
+df <- plumber |>
+  bind_rows(
+    ameriflux
+  ) |>
+  bind_rows(
+    icos_drought2018
+  ) |>
+  bind_rows(
+    icos_warmwinter2020
+  ) |>
+  # bind_rows(
+  #   fluxnet
+  # )
+  group_by(sitename) |>
+  filter(nyears == max(nyears))
 
-df <- full_join(df, oneflux)
+# Complement missing meta information-------------------------------------------
+## Get info from Falge et al https://doi.org/10.3334/ORNLDAAC/1530 -------------
+siteinfo_falge <- read_csv(here::here("data-raw/meta_data/fluxnet_site_info_all_falge.csv")) |>
+  dplyr::select(-sitename) |>
+  dplyr::rename(
+    sitename = fluxnetid
+    ) |>
 
-fluxnet <- fluxnet |>
-  filter(
-    !(sitename %in% df$sitename)
-  )
+  mutate(gtopo30_elevation = as.numeric(gtopo30_elevation)) |>
 
-df <- full_join(df, fluxnet)
+  # split koppen geiger code into separate column
+  dplyr::mutate(koeppen_climate = str_split( koeppen_climate, " - " ) ) |>
+  dplyr::mutate(koeppen_code_falge = purrr::map( koeppen_climate, 1 ) ) |>
+  dplyr::mutate(koeppen_word = purrr::map( koeppen_climate, 2 ) ) |>
+  unnest(c(koeppen_code_falge, koeppen_word)) |>
 
-plumber <- plumber |>
-  filter(
-    !(sitename %in% df$sitename)
-  )
+  # treat IGBP codes
+  mutate(
+    igbp_land_use = dplyr::case_match(
+      igbp_land_use,
+      "Woody Savannas" ~ "WSA",
+      "Savannas" ~ "SAV",
+      "Permanent Wetlands" ~ "WET",
+      "Open Shrublands" ~ "OSH",
+      "Closed Shrublands" ~ "CSH",
+      "Grasslands" ~ "GRA",
+      "Evergreen Needleleaf Forest" ~ "ENF",
+      "Evergreen Broadleaf Forest" ~ "EBF",
+      "Deciduous Broadleaf Forest" ~ "DBF",
+      "Mixed Forests" ~ "MF",
+      "Croplands" ~ "CRO",
+      "Cropland/Natural Vegetation Mosaic" ~ "MF",
+      "Urban and Built-Up" ~ NA,
+      .default = igbp_land_use
+    )
+  ) |>
+  dplyr::select(-koeppen_climate)
 
-df <- full_join(df, plumber)
-
-# fill in end years
 df <- df |>
-  mutate(
-    date_end = ifelse(is.na(date_end), "2021", date_end)
+  arrange(sitename) |>
+  left_join(
+    siteinfo_falge,
+    by = "sitename"
   ) |>
-  filter(
-    !is.na(date_start)
-  )
-
-# convert year_end
-df <- df |>
   mutate(
-    date_start = sprintf("%s-01-01", date_start),
-    date_end = sprintf("%s-12-31", date_end),
-    year_end = format(as.Date(date_end), "%Y"),
-    year_start = format(as.Date(date_start), "%Y")
+    lon = ifelse(is.na(lon), longitude, lon),
+    lat = ifelse(is.na(lat), latitude, lat),
+    elv = ifelse(is.na(elv), gtopo30_elevation, elv),
+    classid = ifelse(is.na(classid), igbp_land_use, classid),
+    koeppen_code = ifelse(is.na(koeppen_code), koeppen_code_falge, koeppen_code)
   )
 
-# set locations for ancillary data extractions
+## Get missing data from ICOS site list file -----------------------------------
+siteinfo_icos <- read_csv(
+  here::here(
+    "data-raw/meta_data/sites_list_icos.csv"
+  )
+)
+
+df <- df |>
+  left_join(
+    siteinfo_icos |>
+      rename(sitename = site),
+    by = "sitename"
+  ) |>
+  mutate(
+    lon = ifelse(is.na(lon), lon_icos, lon),
+    lat = ifelse(is.na(lat), lat_icos, lat),
+    classid = ifelse(is.na(classid), classid_icos, classid)
+  )
+
+## Get still missing koeppen-geiger info from global map------------------------
 loc <- data.frame(lon = df$lon, lat = df$lat)
 
 # get the koeppen geiger values
@@ -159,7 +216,7 @@ kg_key <- read.table(
 # append to original data frame
 df$koeppen_code_beck <- kg_v
 
-# read in igbp labels
+# read in koeppen code labels
 x <- kg_key$class
 names(x) <- kg_key$id
 
@@ -179,48 +236,64 @@ df <- df |>
       koeppen_code)
   )
 
-# get water holding capacity
-whc <- raster("data-raw/ancillary_data/field_capacity/field_capacity.tif")
+## root zone water storage capacity---------------------------------------------
+# using the map from Stocker et al., 2023, obtainable from Zenodo at https://doi.org/10.5281/zenodo.5515246
+whc <- raster("data-raw/ancillary_data/root_zone_water_capacity/cwdx80.nc")
 whc_v <- raster::extract(whc, loc)
 
 # append to original data frame
 df$whc <- whc_v
 
-# download IGBP class values (MODISTools)
-igbp <- apply(df, 1, function(x){
-  mt_subset(
-    lat = x['lat'],
-    lon = x['lon'],
-    product = "MCD12Q1",
-    band = "LC_Type1",
-    start = "2019-01-01",
-    end = "2019-01-01",
-    internal = TRUE,
-    progress = FALSE
-  )$value
-})
+# ## get IGBP from MODIS data-----------------------------------------------------
+# # download IGBP class values (MODISTools)
+# igbp <- apply(df, 1, function(x){
+#   mt_subset(
+#     lat = x['lat'],
+#     lon = x['lon'],
+#     product = "MCD12Q1",
+#     band = "LC_Type1",
+#     start = "2019-01-01",
+#     end = "2019-01-01",
+#     internal = TRUE,
+#     progress = FALSE
+#   )$value
+# })
+#
+# df$igbp_land_use <- igbp
+#
+# igbp_key <- read.table(
+#   "data-raw/ancillary_data/igbp_key.csv",
+#   header = TRUE,
+#   sep = ","
+# )
+#
+# # read in igbp labels
+# x <- igbp_key$class
+# names(x) <- igbp_key$id
+#
+# # rename land cover classes from numeric to
+# # factor (character description)
+# df <- df |>
+#   mutate(
+#     igbp_land_use = recode(igbp_land_use, !!!x)
+#   )
 
-df$igbp_land_use <- igbp
-
-igbp_key <- read.table(
-  "data-raw/ancillary_data/igbp_key.csv",
-  header = TRUE,
-  sep = ","
-)
-
-# read in igbp labels
-x <- igbp_key$class
-names(x) <- igbp_key$id
-
-# rename land cover classes from numeric to
-# factor (character description)
-df <- df |>
-  mutate(
-    igbp_land_use = recode(igbp_land_use, !!!x)
-  )
-
-df <- readRDS("data/flux_data_kit_site-info.rds")
 
 # save the data
-#saveRDS(df, file = "data/flux_data_kit_site-info.rds", compress = "xz")
-save(df, file="data/flux_data_kit_site-info.rda", compress = "xz")
+df <- df |>
+  dplyr::select(
+    sitename,
+    lon,
+    lat,
+    elv,
+    year_start,
+    year_end,
+    koeppen_code,
+    igbp_land_use = classid,
+    whc,
+    product
+  )
+
+# saveRDS(df, file = "data/flux_data_kit_site-info.rds", compress = "xz")
+save(df, file = "data/flux_data_kit_site-info.rda", compress = "xz")
+write_csv(df, file = "data/flux_data_kit_site-info.csv")
